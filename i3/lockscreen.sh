0c01b44c04c8ed127de43c4c326bbaf8ea2e77fe
Nathan Wallace <nwallace6988@gmail.com>
2022-10-07 17:02:05 -0600

    all my configs

---
+++ i3/lockscreen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+i3lock -p default -e -i ~/.config/i3/lockscreen.png
---
+++ i3/polybar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pkill polybar
+pkill cbatticon
+pkill nm-applet
+pkill blueman-applet
+pkill pasystray
+sleep 1
+/usr/bin/nohup cbatticon > /dev/null 2>&1 &
+/usr/bin/nohup nm-applet > /dev/null 2>&1 &
+/usr/bin/nohup blueman-applet > /dev/null 2>&1 &
+/usr/bin/nohup pasystray > /dev/null 2>&1 &
+/usr/bin/nohup polybar > /dev/null 2>&1 &
