9d6e9abe379c15e3d271b6c6ed7e64cabab8876b
Nathan Wallace <nwallace6988@gmail.com>
2022-10-08 19:44:30 -0600

    Don't restart applets that are already running

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 APPLETS="cbatticon nm-applet blueman-applet pasystray"
-ALL_PIDS=()
-
-for A in $APPLETS
-do
-	PIDS=$(ps aux | grep -v grep | grep $USER | grep $A | awk '{print $2}')
-	for P in $PIDS
-	do
-		ALL_PIDS+=( $P )
-	done
-done
-
-for I in ${ALL_PIDS[@]}
-do
-	kill $I
-done
 
 pkill polybar
 polybar &
@@ -22,5 +7,9 @@ polybar &
 sleep 5
 for A in $APPLETS
 do
-	$A &
+	PID=$(ps aux | grep -v grep | grep $USER | grep $A | awk '{print $2}')
+	if [ -z $PID ]
+	then
+		$A &
+	fi
 done
