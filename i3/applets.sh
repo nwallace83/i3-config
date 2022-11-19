c28890234186de7d96abb088641c4caf3a5da1c3
Nathan Wallace <nwallace6988@gmail.com>
2022-11-19 15:28:28 -0700

    sexy up polybar

--- i3/applets.sh
+++ i3/applets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pkill cbatticon
+#pkill cbatticon
 pkill nm-applet
 pkill blueman-applet
 pkill pasystray
 sleep 1
-/usr/bin/nohup cbatticon > /dev/null 2>&1 &
+#/usr/bin/nohup cbatticon > /dev/null 2>&1 &
 /usr/bin/nohup nm-applet > /dev/null 2>&1 &
 /usr/bin/nohup blueman-applet > /dev/null 2>&1 &
 /usr/bin/nohup pasystray > /dev/null 2>&1 &
